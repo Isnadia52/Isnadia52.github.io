a948577da9e573127e6e7f40b7f7229a8caad1cc
Isnadia52 <isnadiyahnurfadhilah@gmail.com>
2025-06-23 18:40:29 +0700

    Update posts-lastmod-hook.rb

--- _plugins/posts-lastmod-hook.rb
+++ _plugins/posts-lastmod-hook.rb
@@ -1,14 +1,23 @@
 #!/usr/bin/env ruby
-#
-# Check for changed posts
+# Plugin: Add last_modified_at to posts using Git history
 
 Jekyll::Hooks.register :posts, :post_init do |post|
+  post_path = post.path
 
-  commit_num = `git rev-list --count HEAD "#{ post.path }"`
+  # Skip if file doesn't exist
+  next unless File.exist?(post_path)
 
-  if commit_num.to_i > 1
-    lastmod_date = `git log -1 --pretty="%ad" --date=iso "#{ post.path }"`
-    post.data['last_modified_at'] = lastmod_date
+  begin
+    # Check number of commits for the file
+    commit_count = git rev-list --count HEAD "#{post_path}".strip.to_i
+
+    if commit_count > 1
+      # Get latest commit date in ISO format
+      last_modified = git log -1 --pretty="%ad" --date=iso "#{post_path}".strip
+      post.data['last_modified_at'] = last_modified
+    end
+
+  rescue => e
+    warn "Git error while processing #{post_path}: #{e.message}"
   end
-
 end
